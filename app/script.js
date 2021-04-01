d2cbb49ff3f514a852feb29506a0303a59fc1e28
Fredrik Welander <fredrik.welander@subsite.fi>
2021-04-01 11:07:25 +0300

    Some commenting

--- app/script.js
+++ app/script.js
@@ -1,13 +1,13 @@
 d3.select('h2').text('Visualisering av information'); 
 
-d3.select('body')
-    .append('div')
-    .text('Hej')
-    .style('color', 'red')
-    .style('opacity', 0)
-    .transition()
-    .duration(4000)
-    .style('opacity', 1)
+d3.select('body')    // Välj  elementet 'body'
+    .append('div')   // Lägg till en div sist inom body
+    .text('Hej')     // Sätt in en text i vår nya div
+    .style('color', 'red') // ändra färg med style()
+    .style('opacity', 0)   // Gör elementet osynligt
+    .transition()          // Skapa en animation till nästa
+    .duration(4000)        // Låt den ta 4 sek
+    .style('opacity', 1)   // Elementet blir långsmt synligt
     
 
     
