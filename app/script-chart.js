b15b5836e2759860a46aa3af6d24cf7aa962f4c3
Fredrik Welander <fredrik.welander@subsite.fi>
2021-04-26 14:32:05 +0300

    Lektion 6

--- app/script-chart.js
+++ app/script-chart.js
@@ -1,82 +1,66 @@
 
+var data = [
+    { label: "Windows", share: 76 },
+    { label: "OSX", share: 16 },
+    { label: "Linux", share: 2 },
+    { label: "Other", share: 8 }
+];
 
-// https://people.arcada.fi/~welandfr/demo/vai/eth-avg.json
+var height = 500, width = 500, margin = 100;
+var radius = Math.min(height, width)/2 - margin;
 
-d3.json('data/eth-avg.json')
-    .then(function(d) {
+var link = '<href="">';
 
-    createChart(d);
-});
+var svg = d3.select('#chart')
+    .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('background', '#efe')
+    .append('g') // Skapa en grupp så att allt innehåll kan flyttas med translate
+        // Cirkelns mittpunkt ska i det här fallet vara samma som svg:ns mittpunkt translate(225,222)
+        .attr('transform', "translate("+width/2+","+height/2+")");
 
-function createChart(rawData) {
+// scaleOrdinal använder vi när vi vill ha en skala som själv är en array med element som motsvarar våra dataelement (t.ex. färger)
+var color = d3.scaleOrdinal()
+    .domain(data)
+    .range(["red", "blue", "lime", "orange", "cyan"]);
 
-    var chartData = [];
-    var dates = [];
-    for (let i = 0; i < rawData.length; i++) {
-        chartData.push(rawData[i].avg);
-        dates.push(new Date(rawData[i].date));
-    }
+// d3.pie() räknar ut positionen för varje datapunkt och genererar
+// nya värden som innehåller det som behövs för att rita diagrammet (t.ex. vinklarna för tårtbitarna)
+var generateSlices = d3.pie()
+    .value(function(d) {
+        return d.value.share;
+    }); 
+// Vi skickar in våra ursprungliga data till pie-generatorn
+var pieData = generateSlices(d3.entries(data));
 
-    console.log(dates);
+svg.selectAll('slices')
+    .data(pieData)
+    .enter().append('path') // svg-path används för att rita t.ex böjda konturer
+        .attr('d', d3.arc() // Skapa "båge", alltså sektorn för kakbiten. startAngle och endAngle kommer från pieData
+            .innerRadius(radius/2)
+            .outerRadius(radius)
+        )
+        .attr('fill', function(d) {
+            console.log(d);
+            return color(d.data.key)
+        })
+        .attr('stroke', 'black')
+        .style('stroke-width', "2px");
 
-    var leftMargin = 40;
-    var bottomMargin = 20;
-    var height = 300;
-    var width = 500;
+var textArcGenerator = d3.arc()
+        .innerRadius(radius+30)
+        .outerRadius(radius+60);
 
-    var chart = d3.select('#chart')
-        .append('svg')
-            .attr('height', height)
-            .attr('width', width)
-            .style('background', '#ddeeff')
-            .append('g')
-                .attr('transform', 'translate('+leftMargin+',0)')
+svg.selectAll('slices')
+        .data(pieData)
+        .enter().append('text')
+            .text(function(d) {
+                return d.data.value.label + " " + d.data.value.share + "%";
+            })
+            .attr('transform', function(d) {
+                return "translate(" + textArcGenerator.centroid(d) + ")";
+            })
+            .style("text-anchor", "middle")
+            .style("font-size", "13px");
 
-    /** Y-axeln */
-    var y = d3.scaleLinear()
-        .domain([d3.min(chartData)*0.95, d3.max(chartData)*1.05]) // Variationen inom vår datamängd
-        .range([0, height]);  // "Utrymmet", var domain 0 och max ska vara
-
-    var yGuideScale = d3.scaleLinear()
-        .domain([d3.min(chartData)*0.95, d3.max(chartData)*1.05]) 
-        .range([height, 0]); 
-
-    var yTicks = d3.axisLeft(yGuideScale)
-        .ticks(10);
-
-    /** X-axeln */ 
-    var x = d3.scaleBand()
-        .domain(chartData)
-        .range([0, width-leftMargin]);
-
-    var xGuideScale = d3.scaleTime()
-        .domain([dates[0], dates[dates.length-1]])
-        .range([0, width-leftMargin]);
-
-    var xTicks = d3.axisBottom(xGuideScale)
-        .ticks(d3.timeWeek)
-        //.ticks(d3.timeDay.every(3));
-    
-    chart.append("path")
-        .datum(chartData)
-            .attr('fill', 'none')
-            .attr('stroke', 'OrangeRed')
-            .attr('stroke-width', 3)
-            .attr('d', d3.line()
-                .x(function(d) {
-                    return x(d);
-                })
-                .y(function(d) {
-                    return height-y(d);
-                })
-            );
-    
-    d3.select('#chart svg').append('g')
-            .attr('transform', 'translate('+leftMargin+',0)')
-            .call(yTicks);
-    
-    d3.select('#chart svg').append('g')
-            .attr('transform', 'translate('+leftMargin+','+(height-bottomMargin)+')')
-            .call(xTicks);
-}
-    
