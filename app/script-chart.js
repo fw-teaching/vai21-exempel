7a1287a89903489621bbfe39b4b516bda95299d3
Fredrik Welander <fredrik.welander@subsite.fi>
2021-05-11 12:35:01 +0300

    Lektion-9

--- app/script-chart.js
+++ app/script-chart.js
@@ -1,130 +1,116 @@
 
 
-d3.json('data/eth-avg.json')
-    .then(function(d) {
+d3.json('data/graph2.json')
+    .then(function (nodes) {
 
-    createChart(d);
-});
+        const links = [];
 
-function createChart(rawData) {
-
-    var chartData = [];
-    var dates = [];
-
-    var chartDataCombo = [];
-    for (let i = 0; i < rawData.length; i++) {
-        chartData.push(rawData[i].avg);
-        dates.push(new Date(rawData[i].date));
-
-        chartDataCombo.push({
-            date: new Date(rawData[i].date).toISOString().substring(0, 10),
-            value: rawData[i].avg
-        });
-    }
-
-    //console.log(chartDataCombo);
-
-    var leftMargin = 40;
-    var bottomMargin = 20;
-    var height = 300;
-    var width = 500;
-
-    var chart = d3.select('#chart')
-        .append('svg')
-            .attr('height', height)
-            .attr('width', width)
-            .style('background', '#ddeeff')
-            .append('g')
-                .attr('transform', 'translate('+leftMargin+',0)')
-
-    /** Y-axeln */
-    var y = d3.scaleLinear()
-        .domain([d3.min(chartData)*0.95, d3.max(chartData)*1.05]) // Variationen inom vår datamängd
-        .range([0, height]);  // "Utrymmet", var domain 0 och max ska vara
-
-    var yGuideScale = d3.scaleLinear()
-        .domain([d3.min(chartData)*0.95, d3.max(chartData)*1.05]) 
-        .range([height, 0]); 
-
-    var yTicks = d3.axisLeft(yGuideScale)
-        .ticks(10);
-
-    /** X-axeln */ 
-    var x = d3.scaleBand()
-        .domain(chartData)
-        .range([0, width-leftMargin]);
-
-    var xGuideScale = d3.scaleTime()
-        .domain([dates[0], dates[dates.length-1]])
-        .range([0, width-leftMargin]);
-
-    var xTicks = d3.axisBottom(xGuideScale)
-        .ticks(d3.timeWeek);
-        //.ticks(d3.timeDay.every(3));
-    
-    chart.append("path")
-        .datum(chartData)
-            .attr('fill', 'none')
-            .attr('stroke', 'crimson')
-            .attr('stroke-width', 3)
-            .attr('d', d3.line()
-                .x(function(d) {
-                    return x(d);
+        for (let i = 0; i < nodes.length; i++) {
+            for (let j = 0; j < nodes[i].links.length; j++) {
+                links.push({
+                    source: nodes[i].id, 
+                    target: nodes[i].links[j]
                 })
-                .y(function(d) {
-                    return height-y(d);
-                })
-            );
+            }
+        }
 
-    var tooltip = d3.select("body").append("div")
+        const d = {nodes: nodes, links: links};
+
+        console.log(d);
+
+        createChart(d);
+    });
+
+function createChart(data) {
+
+    //console.log(data);
+
+    const simulation = d3.forceSimulation(data.nodes)
+        // "laddning" som (om negativ) skjuter noderna från varandra
+        .force('charge', d3.forceManyBody().strength(-100))
+
+        .force('link', d3.forceLink(data.links)
+            .id(d => d.id) // Video om arrow functions: https://youtu.be/h33Srr5J9nY
+            //.id(function(d) { return d.id; })  // samma sak på gammalt sätt
+            .distance(50))
+        
+        // Placering av mittpunkten
+        .force('center', d3.forceCenter(200, 200))
+
+    const svg = d3.select("body").append("svg")
+        .style('background', '#ddeeff')
+        // min-x, min-y, width, height.
+        .attr("viewBox", [0, 0, 600, 600]);
+
+    const link = svg
+        .selectAll('path.link')
+        .data(data.links)
+        .enter()
+        .append('path')
+            .attr('stroke', 'teal')
+            .attr('stroke-width', d => {
+                //console.log(d.source.volume);
+                return (d.source.volume+d.target.volume)*0.05;
+            })
+            .attr('fill', 'none');
+
+    const node = svg.selectAll('circle')
+        .data(data.nodes)
+        .enter()
+        .append('circle')
+            .attr('r', d => {
+                return d.volume/10+5;
+            }) 
+            .attr('fill', 'blue')
+            .attr("fill", "white")
+            .attr("stroke", "orangered")
+        .on('click', (event, d) => console.log(d.name))
+        .on('mouseover', tooltipOpen)
+        .on('mouseout', tooltipClose)
+        .call(d3.drag()
+            .on("start", (event, d) => {
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            })
+            .on("drag", (event, d) => {
+                d.fx = event.x;
+                d.fy = event.y;
+            })
+            .on("end", (event, d) => {
+                d.fx = null;
+                d.fy = null;
+                if (!event.active) simulation.alphaTarget(0);
+            })
+        )
+
+
+
+    const lineGenerator = d3.line();
+
+    simulation.on('tick', () => {
+        node.attr('cx', d => d.x);
+        node.attr('cy', d => d.y);
+        link.attr('d', d => lineGenerator([
+            [d.source.x, d.source.y],
+            [d.target.x, d.target.y]])
+        )
+    });
+
+    const tooltip = d3.select("body").append("div")
         .attr("id", "tooltip")
         .style("opacity", 0);
 
-    chart.selectAll("dot")
-        // .data(chartData) // <-- v5
-        .data(chartDataCombo)
-        .enter().append("circle")
-            .attr("r", 5)
-            .attr("cx", function(d) {
-                return x(d.value);
-            })
-            .attr("cy", function(d) {
-                return height-y(d.value);
-            })
-            .attr("fill", "white")
-            .attr("stroke", "orangered")
-            //v5:
-            /* .on("mouseover", function(d, i) {
-                console.log(d3.event);
-                tooltip.html(dates[i].toISOString().substring(0, 10) + "<br>" + d)
-                    .style("opacity", 0.8)
-                    .style("left", (d3.event.pageX)+10 + "px")
-                    .style("top", (d3.event.pageY)+5 + "px");
-                
-            }) */
-            //v6
-            .on("mouseover", function(event, d) {
-                tooltip.html(d.date + "<br>" + d.value + " €")
-                    .style("opacity", 0.8)
-                    .style("left", (event.pageX)+10 + "px")
-                    .style("top", (event.pageY)+5 + "px");
+    function tooltipOpen(event, d) {
+        tooltip.style("opacity", 0.9);
+        tooltip.html(d.name)
+            .style("left", (event.pageX -20)+ "px")
+            .style("top", (event.pageY -30) + "px");
+    }
+    function tooltipClose(event, d) {
+        console.log("tooltipClose")
+    }
 
-                    d3.select(this).attr("r", 7);
-                
-            })
-            .on("mouseout", function(d) {
-                tooltip.style("opacity", 0);
-
-                d3.select(this).attr("r", 5);
-            });
-
-
-    d3.select('#chart svg').append('g')
-            .attr('transform', 'translate('+(leftMargin)+',0)')
-            .call(yTicks);
     
-    d3.select('#chart svg').append('g')
-            .attr('transform', 'translate('+leftMargin+','+(height-bottomMargin)+')')
-            .call(xTicks);
 }
-    
+
